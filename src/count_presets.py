6368664907e5121263b28920e8b1d86f6a89f21e
itsinfi <sascha.angermann2002@gmail.com>
2025-08-05 19:44:59 +0200

    add file location listing for duplicates

--- src/count_presets.py
+++ src/count_presets.py
@@ -1,14 +1,20 @@
 from config import DIVA_PRESET_DIR
 from utils import get_all_preset_files, get_preset_count, count_duplicate_presets
 
+# TODO: write converter from fxp to h2p
+# TODO: list all counts also per folder
+# TODO: add last preset from freshloops
+# TODO: add preset source listing file to this repository
+
 def main():
     preset_files = get_all_preset_files(preset_dir=DIVA_PRESET_DIR)
 
     preset_count, preset_with_tags_count = get_preset_count(preset_files)
-    duplicates, duplicate_count = count_duplicate_presets(preset_files)
+    duplicates, total_duplicate_count = count_duplicate_presets(preset_files)
 
     print(f'Presets (total): {preset_count}\t\tPresets (w/ tags): {preset_with_tags_count}\t\tPresets (w/o tags): {preset_count - preset_with_tags_count}')
 
-    for duplicate, count in duplicates.items():
-        print(f'Duplicate: {count}x\t{duplicate}')
-    print(f'Total duplicate presets: {duplicate_count}')
+    for i, duplicate in enumerate(duplicates.items()):
+        name, info = duplicate
+        print(f'Duplicate {i + 1}: {info["count"]}x\t{name}\n{info["files"]}\n')
+    print(f'Total duplicate presets: {total_duplicate_count}')
--- src/utils/count_duplicate_presets.py
+++ src/utils/count_duplicate_presets.py
@@ -3,24 +3,38 @@ import re
 def count_duplicate_presets(preset_files: list[str]):
     pattern = r"[\\/]"
 
-    files_names = []
+    file_names = []
     duplicates = {}
-    duplicate_count = 0
+    total_duplicate_count = 0
 
     for preset_file in preset_files:
         split_result = re.split(pattern, string=preset_file)
         file_name = split_result[-1]
-        files_names.append(file_name)
+        file_names.append(file_name)
 
         # in case file name is already a known duplicate (at least 2 occurences)
         if file_name in duplicates:
-            duplicates[file_name] += 1
-            duplicate_count += 1
+            index = file_names.__len__() - 1
+
+            duplicates[file_name]['files'].append(preset_files[index])
+            duplicates[file_name]['count'] += 1
+
+            total_duplicate_count += 1
 
         # in case file name is a first time duplicate (only 2 occurences so far)
-        elif file_name in files_names[:-1]:
-            duplicates[file_name] = 2
-            duplicate_count += 1
+        elif file_name in file_names[:-1]:
+            index_1 = file_names[:-1].index(file_name)
+            index_2 = file_names.__len__() - 1
 
-    return duplicates, duplicate_count
+            duplicates[file_name] = {
+                'count': 2, 
+                'files': [
+                    preset_files[index_1], 
+                    preset_files[index_2]
+                ]
+            }
+
+            total_duplicate_count += 1
+
+    return duplicates, total_duplicate_count
 
